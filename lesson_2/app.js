009da064526543756ca97469ac48e05511e760ac
faraone-prodesign <prodesign@owmedia.ru>
2024-03-13 17:04:42 +0300

    first commit

---
+++ lesson_2/app.js
@@ -0,0 +1,11 @@
+// const   firstName = 'Grigorii'
+// console.log('Hello', firstName)
+
+// let firstName = 'Grigorii'
+// firstName = 'Ivan'
+
+const city = 'Tver'
+const hobbie = 'Music and art'
+const testConst = 'Hello'
+
+console.log('Hello', testConst)
---
+++ lesson_3/app.js
@@ -0,0 +1,31 @@
+const age = 27;
+console.log(typeof age);
+const BigInt = 202902300229349n;
+console.log(typeof BigInt);
+const firstName ='Name';
+console.log(typeof firstName);
+
+let age2;
+console.log(typeof age2);
+
+const obj = {
+    name: 'Grigorii',
+    age: 26,
+    isWorked: true
+}
+
+const num = 11
+const num2 = 5
+
+console.log(num + num2);
+console.log(num - num2);
+console.log(num * num2);
+console.log(num / num2);
+console.log(num % num2);
+
+const titleProject = 'Оценка стоимости';
+const screenValue = "шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = 1000000
+const percentage = 33
+const fullPrice = 5000000
+const responsive = true
