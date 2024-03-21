61c6423eca2eb8ec1e88eb170fdf5311199b8648
faraone-prodesign <prodesign@owmedia.ru>
2024-03-21 12:14:33 +0300

    commit-lesson_4.1

--- lesson_4/lesson-4.js
+++ lesson_4/lesson-4.js
@@ -7,109 +7,34 @@ let service1 = prompt('Какой сервис нужен?');
 let servicePrice1 = prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой ещё сервис нужен?');
 let servicePrice2 = prompt('Сколько будет стоить дополнительный сервис?');
-let fullPrice = screenPrice + +servicePrice1 + +servicePrice2;
 
-// let percent = fullPrice / 100 * 15
-// let servicePercentPrice = fullPrice - percent
-// console.log(servicePercentPrice.toString())
+function initCalc() {
 
-// let x = fullPrice
-
-// if (x > 50000) {
-//     console.log('Ваша скидка 10%');
-// } else if (x > 20000 && x < 50000) {
-//     console.log('Ваша скидка 5%'); 
-// } else if (x > 0 && x < 20000) {
-//     console.log('Спасибо за покупку!'); 
-// } else if (x < 0) {
-//     console.log('Page not found');
-// } else if (x === 0 || x === 20000 || x === 50000) {
-//     console.log('Проверка на строгое равенство');
-// }
-
-// console.log(titleProject, screensValue, responsive, service1, servicePrice1, service2, servicePrice2, fullPrice, servicePercentPrice);
-
-'use strict'
-
-// let getAllServicePrices = function() {
-//     allServicePrices = +servicePrice1 + +servicePrice2;
-//     console.log(allServicePrices);
-// };
-
-// getAllServicePrices()
-
-// function getFullPrice() {
-//     let fullPrice = allServicePrices + screenPrice;
-//     console.log(fullPrice);
-// }
-
-// getFullPrice()
-
-// function getTitle() {
-//     let result = titleProject.charAt(0).toUpperCase() + titleProject.slice(1).toLowerCase();
-//     console.log(result);
-// };
-
-// getTitle()
-
-// function getServicePercentPrices() {
-//     let percent = fullPrice / 100 * 15;
-//     let servicePercentPrice = fullPrice - percent;
-//     console.log(servicePercentPrice.toString());
-// };
-
-// getServicePercentPrices()
-
-// function costCalculator() {
-//     let x = fullPrice
-
-//     if (x > 50000) {
-//         console.log('Ваша скидка 10%');
-//     } else if (x > 20000 && x < 50000) {
-//         console.log('Ваша скидка 5%'); 
-//     } else if (x > 0 && x < 20000) {
-//         console.log('Спасибо за покупку!'); 
-//     } else if (x < 0) {
-//         console.log('Page not found');
-//     } else if (x === 0 || x === 20000 || x === 50000) {
-//         console.log('Проверка на строгое равенство');
-//     }
-
-// }
-
-// costCalculator()
-
-function getRollbackMessage() {
     let getAllServicePrices = function() {
-        allServicePrices = +servicePrice1 + +servicePrice2;
-        console.log(allServicePrices);
+        return +servicePrice1 + +servicePrice2;
     };
-    
-    getAllServicePrices()
-    
+    let allServicePrices = getAllServicePrices();
+
     function getFullPrice() {
-        let fullPrice = allServicePrices + screenPrice;
-        console.log(fullPrice);
+        return allServicePrices + screenPrice;
     }
-    
-    getFullPrice()
-    
+    let fullPrice = getFullPrice();
+
     function getTitle() {
-        let result = titleProject.charAt(0).toUpperCase() + titleProject.slice(1).toLowerCase();
-        console.log(result);
+        return titleProject.charAt(0).toUpperCase() + titleProject.slice(1).toLowerCase();
     };
-    
-    getTitle()
-    
+
+    titleProject = getTitle();
+
     function getServicePercentPrices() {
         let percent = fullPrice / 100 * 15;
-        let servicePercentPrice = fullPrice - percent;
-        console.log(servicePercentPrice.toString());
+        return fullPrice - percent;
     };
     
-    getServicePercentPrices()
-    
-    function costCalculator() {
+    let servicePercentPrice = getServicePercentPrices();
+    console.log(servicePercentPrice);
+
+    function getRollbackMessage() {
         let x = fullPrice
     
         if (x > 50000) {
@@ -126,7 +51,7 @@ function getRollbackMessage() {
     
     }
     
-    costCalculator()
+    getRollbackMessage()
 }
 
-getRollbackMessage()
+initCalc()
